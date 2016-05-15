1927640236c10d4e0735908d6174e1474ebd1940
Mikey Lee <mikey1384@gmail.com>
2016-05-15 21:55:37 +0900

    webpack modified

--- webpack.prod.config.js
+++ webpack.prod.config.js
@@ -10,7 +10,7 @@ module.exports = {
     extensions:         ['', '.js', '.jsx']
   },
   output: {
-    path: path.join(__dirname, 'dist'),
+    path: path.join(__dirname + '/entry/', 'dist'),
     filename: 'bundle.js',
     chunkFilename: '[id].chunk.js',
     publicPath: '/'
