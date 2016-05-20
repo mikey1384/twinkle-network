038f51327e1f7575d4e5bad84b21832ba8710e61
Mikey Lee <mikey1384@gmail.com>
2016-05-20 09:33:27 +0900

    edit comment textarea added

---
+++ shared/containers/Contents/VideoPage/Comments/EditCommentTextArea.js
@@ -0,0 +1,50 @@
+import React, { Component } from 'react';
+import Textarea from 'react-textarea-autosize';
+
+export default class EditCommentTextArea extends Component {
+  state = {
+    editedText: this.props.text
+  }
+  render() {
+    return (
+      <div>
+        <Textarea
+          autoFocus
+          className="form-control"
+          style={{
+            marginTop: '1em'
+          }}
+          rows={4}
+          value={this.state.editedText}
+          onChange={ e => this.setState({editedText: e.target.value}) }
+        />
+        <div
+          style={{
+            marginTop: '1em'
+          }}
+        >
+          <button
+            className="btn btn-default btn-sm"
+            onClick={ this.onSubmit.bind(this) }
+          >
+            Done
+          </button>
+          <button
+            className="btn btn-default btn-sm"
+            style={{
+              marginLeft: '0.5em'
+            }}
+            onClick={ () => this.props.onCancel() }
+          >
+            Cancel
+          </button>
+        </div>
+      </div>
+    )
+  }
+
+  onSubmit() {
+    const { editedText } = this.state;
+    this.props.onEditDone(editedText);
+  }
+}
