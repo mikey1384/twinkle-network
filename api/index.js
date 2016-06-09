52c21a2c45227fc4def00e719488fba89ffde604
Mikey Lee <mikey1384@gmail.com>
2016-06-09 23:12:00 +0900

    minor api change

--- api/index.js
+++ api/index.js
@@ -9,7 +9,6 @@ const app = express();
 app.use(cors());
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
-app.use(siteSession());
 
 router(app);
 const http = require('http').Server(app);
