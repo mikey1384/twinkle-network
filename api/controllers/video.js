d8514509a582f567e022e7c3e10bdda7d8ece69c
Mikey Lee <mikey1384@gmail.com>
2016-07-15 21:44:28 +0900

    deleted api helpers

--- api/controllers/video.js
+++ api/controllers/video.js
@@ -5,7 +5,6 @@ const processedString = require('../helpers/StringHelper').processedString;
 const processedTitleString = require('../helpers/StringHelper').processedTitleString;
 const fetchedVideoCodeFromURL = require('../helpers/StringHelper').fetchedVideoCodeFromURL;
 const stringIsEmpty = require('../helpers/StringHelper').stringIsEmpty;
-const returnComments = require('../helpers/VideoHelper').returnComments;
 
 const async = require('async');
 const express = require('express');
@@ -559,5 +558,115 @@ router.post('/questions', requireAuth, (req, res) => {
     res.json({success});
   });
 })
+
+const fetchCommentElements = (params) => cb => {
+  let commentRow = params.commentRow;
+  let commentsArray = params.commentsArray;
+  let index = params.index;
+  let commentId = commentRow.id;
+  async.parallel([
+    callback => {
+      let query = [
+        'SELECT a.id, a.userid, b.username FROM ',
+        'vq_commentupvotes a JOIN users b ON ',
+        'a.userid = b.id WHERE ',
+        'a.commentid = ?'
+      ].join('')
+      pool.query(query, commentId, (err, rows) => {
+        callback(err, rows);
+      })
+    },
+    callback => {
+      let query = [
+        'SELECT a.id, a.userid, a.content, a.timeposted, b.username FROM ',
+        'vq_replies a JOIN users b ON ',
+        'a.userid = b.id WHERE ',
+        'a.commentid = ?'
+      ].join('')
+      pool.query(query, commentId, (err, rows) => {
+        returnReplies(rows, (err, repliesArray) => {
+          callback(err, repliesArray);
+        })
+      })
+    }
+  ], (err, results) => {
+    const likes = results[0].map(like => {
+      return {
+        userId: like.userid,
+        username: like.username
+      }
+    });
+    const replies = results[1];
+    commentsArray[index] = {
+      id: commentId,
+      posterId: commentRow.userid,
+      posterName: commentRow.username,
+      content: commentRow.content,
+      timeStamp: commentRow.timeposted,
+      likes,
+      replies
+    }
+    cb(err);
+  })
+}
+
+function returnReplies(replyRows, cb) {
+  let repliesArray = [];
+  let taskArray = [];
+  if (replyRows.length === 0) {
+    return cb(null, []);
+  }
+  for (let index = 0; index < replyRows.length; index++) {
+    let replyRow = replyRows[index];
+    taskArray.push(fetchReplyElements({replyRow, repliesArray, index}));
+  }
+  async.parallel(taskArray, err => {
+    cb(err, repliesArray)
+  });
+}
+
+const fetchReplyElements = (params) => cb => {
+  let replyRow = params.replyRow;
+  let repliesArray = params.repliesArray;
+  let index = params.index;
+  let replyId = replyRow.id;
+  let query = [
+    'SELECT a.userid, b.username ',
+    'FROM vq_replyupvotes a JOIN users b ON ',
+    'a.userid = b.id WHERE ',
+    'a.replyid = ?'
+  ].join('')
+  pool.query(query, replyId, (err, rows) => {
+    repliesArray[index] = {
+      id: replyId,
+      content: replyRow.content,
+      timeStamp: replyRow.timeposted,
+      userId: replyRow.userid,
+      username: replyRow.username,
+      likes: rows.map(like => {
+        return {
+          userId: like.userid,
+          username: like.username
+        }
+      })
+    }
+    cb(err);
+  })
+}
+
+function returnComments(commentRows, cb) {
+  let commentsArray = [];
+  let taskArray = [];
+  if (commentRows.length === 0) {
+    return cb(null, []);
+  }
+  for (let index = 0; index < commentRows.length; index++) {
+    let commentRow = commentRows[index];
+    taskArray.push(fetchCommentElements({commentRow, commentsArray, index}));
+  }
+  async.parallel(taskArray, err => {
+    cb(err, commentsArray)
+  });
+}
 
 module.exports = router;
