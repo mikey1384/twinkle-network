6b47f73d09552dbed2121a87207fb96114bfad2d
Mikey Lee <mikey1384@gmail.com>
2016-07-15 19:53:36 +0900

    added pool

---
+++ api/pool.js
@@ -0,0 +1,14 @@
+const config = require('./siteConfig');
+const mysql = require('mysql');
+const pool = mysql.createPool({
+  connectionLimit: 10,
+  host: 'localhost',
+  user: config.mysqlUser,
+  password: config.mysqlPassword,
+  database: config.mysqlDatabase,
+  supportBigNumbers: true,
+  bigNumberStrings: true,
+  debug: false
+})
+
+module.exports = pool;
