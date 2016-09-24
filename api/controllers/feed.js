a4125899f6fffe7b99a787963c3c605c11c0cd1c
Mikey Lee <mikey1384@gmail.com>
2016-09-24 11:30:21 +0900

    minor bug fix

--- api/controllers/feed.js
+++ api/controllers/feed.js
@@ -11,7 +11,7 @@ router.get('/', (req, res) => {
     'SELECT a.id AS contentId, a.userId AS uploaderId, a.content, a.timeStamp, a.videoId AS parentContentId, a.commentId, ', 'b.title AS parentContentTitle, ',
     'c.username AS uploaderName, d.userId AS siblingContentUploaderId, d.content AS siblingContent, ',
     'e.username AS siblingContentUploaderName ',
-    'FROM vq_comments a vq_videos b ON a.videoId = b.id JOIN users c ON a.userId = c.id ',
+    'FROM vq_comments a JOIN vq_videos b ON a.videoId = b.id JOIN users c ON a.userId = c.id ',
     'LEFT JOIN vq_comments d ON a.commentId = d.id LEFT JOIN users e ON d.userId = e.id ',
     'ORDER BY a.timeStamp DESC LIMIT ' + limit
   ].join('')
