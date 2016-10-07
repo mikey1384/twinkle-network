3362c321f550831485518e43a6c5727840914272
Mikey Lee <mikey1384@gmail.com>
2016-10-07 16:14:00 +0900

    liked is no longer yello

--- shared/components/LikeButton.js
+++ shared/components/LikeButton.js
@@ -2,13 +2,10 @@ import React from 'react';
 import Button from 'components/Button';
 
 export default function LikeButton({style, liked, onClick, small}) {
-  let buttonStyle = Object.assign({}, style, {
-    color: liked ? 'yellow' : '#fff'
-  })
   return (
     <Button
       className={`btn btn-${liked ? 'primary' : 'info'}${small ? ' btn-sm' : ''}`}
-      style={buttonStyle}
+      style={style}
       onClick={onClick}
     >
       <span className="glyphicon glyphicon-thumbs-up"></span> {`${liked ? 'Liked!' : 'Like'}`}
