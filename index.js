d6669d0aac6e7d1c413c7c74d068d5c362a57bd8
Mikey Lee <mikey1384@gmail.com>
2016-10-17 17:39:08 +0900

    revert to original

--- index.js
+++ index.js
@@ -4,23 +4,8 @@ require('babel-core/register')({});
 require('babel-polyfill');
 var http = require('http');
 var server = require('./entry/server').default;
-var TestPort = process.env.PORT;
+const PORT = process.env.PORT || 80;
 
-if (!!TestPort) {
-  return http.createServer(server).listen(TestPort, function () {
-    console.log('Server listening on: ' + TestPort);
-  });
-}
-
-require('letsencrypt-express').create({
-
-  server: 'staging',
-  email: 'twinkle.andrew@gmail.com',
-  agreeTos: true,
-  approveDomains: [ 'twin-kle.com' ]
-
-, app: server
-
-}).listen(80, 443, function() {
-  console.log('Server listening on port 80 and 443');
+http.createServer(server).listen(PORT, function () {
+  console.log('Server listening on: ' + PORT);
 });
