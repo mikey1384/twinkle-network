e5606398fa78d5abbfb3bfa71a6332a4ae41f000
Mikey Lee <mikey1384@gmail.com>
2017-02-19 17:58:03 +0900

    webpack bundles properly now

--- webpack/webpack.prod.config.js
+++ webpack/webpack.prod.config.js
@@ -9,7 +9,7 @@ module.exports = {
   },
   output: {
     filename: 'bundle.js',
-    path: path.join(__dirname, 'public')
+    path: path.join(__dirname, '/../public')
   },
   module: {
     rules: [{
