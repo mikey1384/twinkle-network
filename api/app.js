c86adf4231faf15e371e90f6645c966690e39007
Mikey Lee <mikey1384@gmail.com>
2017-02-20 12:56:34 +0900

    search box width reduced

--- api/app.js
+++ api/app.js
@@ -8,13 +8,6 @@ app.use(cors())
 app.use(bodyParser.urlencoded({extended: false}))
 app.use(bodyParser.json({limit: '5mb'}))
 
-/*
-const options = {
-  key: fs.readFileSync('./certs/server/privkey.pem'),
-  cert: fs.readFileSync('./certs/certificate.pem')
-}
-*/
-
 router(app)
 
 module.exports = app
