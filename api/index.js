147357f685a50a59e68654dfabe5f657a834ca5e
Mikey Lee <mikey1384@gmail.com>
2017-02-22 03:03:27 +0900

    https applied

--- api/index.js
+++ api/index.js
@@ -1,7 +1,7 @@
 const app = require('./app')
 const socket = require('./socket')
 const lex = require('greenlock-express').create({
-  server: 'staging', // https://acme-v01.api.letsencrypt.org/directory
+  server: 'https://acme-v01.api.letsencrypt.org/directory',
   email: 'mikey1384@gmail.com',
   agreeTos: true,
   approveDomains: ['www.twin-kle.com', 'twin-kle.com']
