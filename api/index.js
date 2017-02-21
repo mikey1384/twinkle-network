16a60ca9929b54c57363c1e9cd3257da003b01d1
Mikey Lee <mikey1384@gmail.com>
2017-02-22 02:23:40 +0900

    https test

--- api/index.js
+++ api/index.js
@@ -1,17 +1,19 @@
 const app = require('./app')
 const socket = require('./socket')
+const lex = require('greenlock-express').create({
+  server: 'staging',
+  email: 'mikey1384@gmail.com',
+  agreeTos: true,
+  approveDomains: ['www.stage5society.com']
+})
 
 if (!process.env.PORT) {
-  const http = require('http').Server(app)
-  const io = require('socket.io')(http)
+  const https = require('https').createServer(lex.httpsOptions, lex.middleware(app))
+  const io = require('socket.io')(https)
   socket(io)
-  require('greenlock-express').create({
-    server: 'staging',
-    email: 'mikey1384@gmail.com',
-    agreeTos: true,
-    approveDomains: ['www.stage5society.com'],
-    app: http
-  }).listen(3500)
+  https.listen(3500, function() {
+    console.log('Listening for ACME tls-sni-01 challenges and serve app on', 3500)
+  })
 } else {
   const http = require('http').Server(app)
 
