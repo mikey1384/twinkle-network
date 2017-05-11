a2eb77be0b08ba98c08c8128413546750a7cea48
Mikey Lee <mikey1384@gmail.com>
2017-05-11 16:44:10 +0800

    version change

--- api/controllers/notification.js
+++ api/controllers/notification.js
@@ -2,7 +2,7 @@ const express = require('express')
 const router = express.Router()
 const {requireAuth} = require('../auth')
 const {poolQuery} = require('../helpers')
-const currentVersion = 0.06
+const currentVersion = 0.07
 
 router.get('/', requireAuth, (req, res) => {
   const {id: userId} = req.user
