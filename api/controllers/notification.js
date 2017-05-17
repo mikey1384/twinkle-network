6ba851a65bad769a712ee5b60bca69446e0d7cf2
Mikey Lee <mikey1384@gmail.com>
2017-05-18 02:02:49 +0900

    video fetch algorithm change

--- api/controllers/notification.js
+++ api/controllers/notification.js
@@ -2,7 +2,7 @@ const express = require('express')
 const router = express.Router()
 const {requireAuth} = require('../auth')
 const {poolQuery} = require('../helpers')
-const currentVersion = 0.12
+const currentVersion = 0.13
 
 router.get('/', requireAuth, (req, res) => {
   const {id: userId} = req.user
