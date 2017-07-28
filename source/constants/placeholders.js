4ff5b22979d6a6ca3fceb7e7e3aa3cf75f1fa79a
Mikey Lee <mikey1384@gmail.com>
2017-07-29 00:21:23 +0900

    chat api bug fix

---
+++ source/constants/placeholders.js
@@ -0,0 +1,8 @@
+export const edit = {
+  comment: 'Enter Comment...',
+  description: 'Enter Description... (Optional)',
+  title: 'Enter Title...',
+  url: 'Enter URL...',
+  subject: 'Enter Subject...',
+  video: 'Enter YouTube URL...'
+}
