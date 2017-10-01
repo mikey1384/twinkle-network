ff26933c16578c4a912f03f6b9c17e4858fe4aee
Mikey Lee <mikey1384@gmail.com>
2017-10-01 22:58:20 +0900

    additional domains approve

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ require('babel-polyfill')
 const server = require('./entry/server').default
 if (process.env.NODE_ENV === 'production') {
   require('greenlock-express').create({
-    server: 'https://acme-v01.api.letsencrypt.org/directory',
+    server: 'staging',
     email: 'mikey1384@gmail.com',
     agreeTos: true,
     approveDomains: ['www.twinkle.network', 'twinkle.network'],
