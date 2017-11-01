6779c4d26d0649f7785b9bf7da098da52471d02d
Mikey Lee <mikey1384@gmail.com>
2017-11-01 14:21:00 +0900

    version up

--- source/redux/actions/NotiActions.js
+++ source/redux/actions/NotiActions.js
@@ -3,7 +3,7 @@ import {auth, handleError} from './constants'
 import {URL} from 'constants/URL'
 
 const API_URL = `${URL}/notification`
-const appVersion = 0.058
+const appVersion = '0.0.59'
 
 export const checkVersion = () => dispatch =>
   request.get(`${API_URL}/version?version=${appVersion}`).then(
