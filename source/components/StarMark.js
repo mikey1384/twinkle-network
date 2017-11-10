2ad017d5e25ebdc9413836293a417a579aae37d5
Mikey Lee <mikey1384@gmail.com>
2017-11-10 22:52:43 +0900

    starmark added

---
+++ source/components/StarMark.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+StarMark.propTypes = {
+  size: PropTypes.number,
+  style: PropTypes.object
+}
+export default function StarMark({style, size = 4}) {
+  return (
+    <img
+      style={{
+        ...style,
+        width: `${size}vw`,
+        height: `${size}vw`,
+        position: 'absolute'
+      }}
+      src={'/img/star.png'}
+    />
+  )
+}
