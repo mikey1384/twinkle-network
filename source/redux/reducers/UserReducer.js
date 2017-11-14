7030b707595dcbc1f521f348d44616a69ecfdea9
Mikey Lee <mikey1384@gmail.com>
2017-11-14 21:27:21 +0900

    new admin labels

--- source/redux/reducers/UserReducer.js
+++ source/redux/reducers/UserReducer.js
@@ -1,5 +1,6 @@
 const defaultState = {
   isAdmin: false,
+  isCreator: false,
   profile: {},
   profiles: [],
   searchedProfiles: [],
@@ -7,7 +8,8 @@ const defaultState = {
 }
 
 function isAdmin(userType) {
-  return userType === 'master'
+  const admin = ['manager', 'creator']
+  return admin.indexOf(userType) !== -1
 }
 
 export default function UserReducer(state = defaultState, action) {
@@ -22,7 +24,8 @@ export default function UserReducer(state = defaultState, action) {
       return (action.data !== undefined && action.data.loggedIn) ? {
         ...state,
         ...action.data,
-        isAdmin: isAdmin(action.data.userType)
+        isAdmin: isAdmin(action.data.userType),
+        isCreator: action.data.userType === 'creator'
       } : state
     case 'FETCH_USERS':
       if (action.data.length > 20) {
@@ -69,10 +72,13 @@ export default function UserReducer(state = defaultState, action) {
         ...action.data,
         loggedIn: true,
         signinModalShown: false,
-        isAdmin: isAdmin(action.data.userType)
+        isAdmin: isAdmin(action.data.userType),
+        isCreator: action.data.userType === 'creator'
       }
     case 'SIGNIN_LOGOUT':
       return {
+        isAdmin: false,
+        isCreator: false,
         profile: state.profile,
         profiles: state.profiles
       }
@@ -81,6 +87,7 @@ export default function UserReducer(state = defaultState, action) {
         ...state,
         ...action.data,
         isAdmin: isAdmin(action.data.userType),
+        isCreator: action.data.userType === 'creator',
         loggedIn: true,
         signinModalShown: false
       }
