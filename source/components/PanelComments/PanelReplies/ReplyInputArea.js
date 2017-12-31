ac476947d213834a8920313f27b223f36f1ebd9f
mikey1384 <mikey1384@gmail.com>
2017-12-31 12:45:40 +0900

    panel comments component flexified

--- source/components/PanelComments/PanelReplies/ReplyInputArea.js
+++ source/components/PanelComments/PanelReplies/ReplyInputArea.js
@@ -4,25 +4,18 @@ import InputArea from 'components/Texts/InputArea'
 
 ReplyInputArea.propTypes = {
   onSubmit: PropTypes.func.isRequired,
-  numReplies: PropTypes.number,
   clickListenerState: PropTypes.bool
 }
-export default function ReplyInputArea({
-  onSubmit,
-  numReplies,
-  clickListenerState
-}) {
+export default function ReplyInputArea({ onSubmit, clickListenerState }) {
   return (
-    <div className="media" style={{ marginTop: numReplies === 0 && '0px' }}>
-      <div className="media-body">
-        <InputArea
-          autoFocus
-          clickListenerState={clickListenerState}
-          onSubmit={text => onSubmit(text)}
-          rows={4}
-          placeholder="Post your reply..."
-        />
-      </div>
+    <div style={{ marginTop: '1rem' }}>
+      <InputArea
+        autoFocus
+        clickListenerState={clickListenerState}
+        onSubmit={text => onSubmit(text)}
+        rows={4}
+        placeholder="Post your reply..."
+      />
     </div>
   )
 }
