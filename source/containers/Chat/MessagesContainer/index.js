0a1e6cdc285205ba42f6a9527c4b9ea7cb112472
mikey1384 <mikey1384@gmail.com>
2018-01-04 21:28:50 +0900

    scroll bug fix

--- source/containers/Chat/MessagesContainer/index.js
+++ source/containers/Chat/MessagesContainer/index.js
@@ -77,7 +77,7 @@ export default class MessagesContainer extends Component {
     scrollBottom.bind(this)()
     setTimeout(() => {
       scrollBottom.bind(this)()
-    }, 10)
+    }, 50)
 
     function scrollBottom() {
       this.messagesContainer.scrollTop = Math.max(
