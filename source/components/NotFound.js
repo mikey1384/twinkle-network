47f1bdbf5c1ff392d0c9c8c3f4f22445ca6d01d3
mikey1384 <mikey1384@gmail.com>
2018-01-26 13:18:52 +0700

    create comment page container

--- source/components/NotFound.js
+++ source/components/NotFound.js
@@ -7,7 +7,7 @@ NotFound.propTypes = {
 }
 export default function NotFound({ title, text }) {
   return (
-    <div className="container">
+    <div style={{ textAlign: 'center' }}>
       <h1>{title || 'Not Found'}</h1>
       <p>{text || 'The page you requested does not exist'}</p>
     </div>
--- source/containers/App/index.js
+++ source/containers/App/index.js
@@ -14,6 +14,7 @@ import { changePageVisibility } from 'redux/actions/ViewActions'
 import { initSessionAsync } from 'redux/actions/UserActions'
 import { addEvent, removeEvent } from 'helpers/listenerHelpers'
 import Home from 'containers/Home'
+import CommentPage from 'containers/CommentPage'
 import Videos from 'containers/Videos'
 import Links from 'containers/Links'
 import TwinkleXP from 'containers/TwinkleXP'
@@ -191,6 +192,7 @@ class App extends Component {
           )}
           <Switch>
             <Route exact path="/" component={Home} />
+            <Route path="/comments" component={CommentPage} />
             <Route path="/videos" component={Videos} />
             <Route path="/links" component={Links} />
             <Route path="/users" component={Home} />
