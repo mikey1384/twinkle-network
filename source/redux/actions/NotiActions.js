a33bb00f19f3ffaf00865e0a3e4f724b2e2652e4
mikey1384 <mikey1384@gmail.com>
2018-02-07 11:34:05 +0700

    implemented comment link to noti feeds

--- source/redux/actions/NotiActions.js
+++ source/redux/actions/NotiActions.js
@@ -3,7 +3,7 @@ import { auth, handleError } from './constants'
 import { URL } from 'constants/URL'
 
 const API_URL = `${URL}/notification`
-const appVersion = '0.0.77'
+const appVersion = '0.0.78'
 
 export const checkVersion = () => dispatch =>
   request
