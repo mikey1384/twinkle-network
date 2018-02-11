a2da23985d86be92fc08f049913e7c0502b85e9f
mikey1384 <mikey1384@gmail.com>
2018-02-11 17:45:23 +0700

    implement new scrolling system

---
+++ source/containers/Links/Main/Styles.js
@@ -0,0 +1,15 @@
+import { css } from 'react-emotion'
+
+export const Left = css`
+  width: CALC(70% - 3rem);
+  margin-left: 1rem;
+`
+
+export const Right = css`
+  position: absolute;
+  right: 1rem;
+  top: 65px;
+  bottom: 0;
+  width: 30%;
+  overflow-y: scroll;
+`
