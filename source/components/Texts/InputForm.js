15002628de4a2c52335b44e75d1752484aeae5dc
mikey1384 <mikey1384@gmail.com>
2018-02-22 17:08:49 +0700

    more css improvements

---
+++ source/components/Texts/InputForm.js
@@ -0,0 +1,94 @@
+import PropTypes from 'prop-types'
+import React, { Component } from 'react'
+import Button from 'components/Button'
+import Textarea from 'components/Texts/Textarea'
+import { stringIsEmpty, addEmoji, finalizeEmoji } from 'helpers/stringHelpers'
+import { scrollElementToCenter } from 'helpers/domHelpers'
+
+export default class InputForm extends Component {
+  static propTypes = {
+    autoFocus: PropTypes.bool,
+    clickListenerState: PropTypes.bool,
+    formGroupStyle: PropTypes.object,
+    onSubmit: PropTypes.func.isRequired,
+    placeholder: PropTypes.string,
+    rows: PropTypes.number
+  }
+
+  state = {
+    text: ''
+  }
+
+  componentDidUpdate(prevProps) {
+    if (prevProps.clickListenerState !== this.props.clickListenerState) {
+      let nodes = this.InputArea.childNodes
+      for (let i = 0; i < nodes.length; i++) {
+        if (nodes[i].tagName === 'TEXTAREA') {
+          nodes[i].focus()
+          scrollElementToCenter(nodes[i])
+        }
+      }
+    }
+  }
+
+  render() {
+    const { text } = this.state
+    const {
+      placeholder,
+      rows,
+      autoFocus,
+      formGroupStyle = {},
+      ...props
+    } = this.props
+    return (
+      <div {...props}>
+        <div
+          style={{
+            position: 'relative',
+            ...formGroupStyle
+          }}
+          ref={ref => {
+            this.InputArea = ref
+          }}
+        >
+          <Textarea
+            autoFocus={autoFocus}
+            style={{ fontSize: '1.7rem' }}
+            minRows={rows}
+            value={text}
+            placeholder={placeholder}
+            onChange={event => this.setState({ text: event.target.value })}
+            onKeyUp={this.handleKeyUp}
+          />
+        </div>
+        <div
+          css={`
+            display: flex;
+            flex-direction: row-reverse;
+          `}
+        >
+          <Button
+            style={{ marginTop: '1rem' }}
+            filled
+            success
+            disabled={stringIsEmpty(text)}
+            onClick={this.onSubmit}
+          >
+            Click This Button to Submit!
+          </Button>
+        </div>
+      </div>
+    )
+  }
+
+  handleKeyUp = event => {
+    if (event.key === ' ') this.setState({ text: addEmoji(event.target.value) })
+  }
+
+  onSubmit = () => {
+    if (!stringIsEmpty(this.state.text)) {
+      this.props.onSubmit(finalizeEmoji(this.state.text))
+      this.setState({ text: '' })
+    }
+  }
+}
---
+++ source/components/Texts/Textarea.js
@@ -0,0 +1,34 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import { Color } from 'constants/css'
+import TextareaAutosize from 'react-textarea-autosize'
+
+Textarea.propTypes = {
+  innerRef: PropTypes.func
+}
+export default function Textarea({ innerRef, ...props }) {
+  return (
+    <TextareaAutosize
+      {...props}
+      ref={innerRef}
+      css={`
+        width: 100%;
+        position: relative;
+        font-size: 1.6rem;
+        padding: 1rem;
+        border: 1px solid ${Color.inputBorderGray()};
+        &:focus {
+          outline: none;
+          border: 1px solid ${Color.logoBlue()};
+          box-shadow: 0px 0px 3px ${Color.logoBlue(0.8)};
+          ::placeholder {
+            color: ${Color.lightGray()};
+          }
+        }
+        ::placeholder {
+          color: ${Color.gray()};
+        }
+      `}
+    />
+  )
+}
