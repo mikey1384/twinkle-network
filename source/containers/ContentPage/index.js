bdcf27bf9612726fdbdeb2cdd703db93f6e0a496
mikey1384 <mikey1384@gmail.com>
2018-03-15 12:41:39 +0700

    content page bug fix

--- source/containers/ContentPage/index.js
+++ source/containers/ContentPage/index.js
@@ -3,15 +3,30 @@ import PropTypes from 'prop-types'
 import { Route, Switch } from 'react-router-dom'
 import NotFound from 'components/NotFound'
 import Content from './Content'
-import { contentPage } from './Styles'
+import { css } from 'emotion'
+import { mobileMaxWidth } from 'constants/css'
 
 CommentPage.propTypes = {
   match: PropTypes.object.isRequired
 }
 export default function CommentPage({ match }) {
   return (
-    <div className={contentPage}>
-      <section>
+    <div
+      className={css`
+        width: 100%;
+        display: flex;
+        justify-content: center;
+      `}
+      style={{ marginBottom: '1rem' }}
+    >
+      <section
+        className={css`
+          width: 65%;
+          @media (max-width: ${mobileMaxWidth}) {
+            width: 100%;
+          }
+        `}
+      >
         <Switch>
           <Route exact path={`${match.url}/:contentId`} component={Content} />
           <Route component={NotFound} />
