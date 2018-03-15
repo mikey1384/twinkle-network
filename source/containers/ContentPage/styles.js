8f58a78abf4d34eb9ea5f05c8fa73092d7a642a5
mikey1384 <mikey1384@gmail.com>
2018-03-15 12:15:10 +0700

    minor content page css change

--- source/containers/ContentPage/styles.js
+++ source/containers/ContentPage/styles.js
@@ -5,6 +5,7 @@ export const contentPage = css`
   width: 100%;
   display: flex;
   justify-content: center;
+  margin-bottom: 1rem;
   > section {
     width: 65%;
     @media (max-width: ${mobileMaxWidth}) {
