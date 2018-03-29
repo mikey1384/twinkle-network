83c18d5188641db6c2593c450d8a511cdc1fbacf
mikey1384 <mikey1384@gmail.com>
2018-03-29 23:01:25 +0700

    implement realtime home feed update

---
+++ entry/middlewares/notifier.js
@@ -0,0 +1,23 @@
+import { socket } from '../../source/constants/io'
+
+const validActors = ['VIDEO', 'FEED', 'LINK']
+
+function createNotifier() {
+  return () => next => action => {
+    const { type } = action
+    if (type) {
+      const typeKeyArray = type.split('_')
+      const actionKey = typeKeyArray[0]
+      const actorKey = typeKeyArray[typeKeyArray.length - 1]
+      if (validActors.indexOf(actorKey) !== -1 && actionKey === 'UPLOAD') {
+        socket.emit('new_upload')
+      }
+    }
+    return next(action)
+  }
+}
+
+const notifier = createNotifier()
+
+export default notifier
+
--- source/redux/actions/NotiActions.js
+++ source/redux/actions/NotiActions.js
@@ -4,7 +4,7 @@ import { URL } from 'constants/URL'
 import NOTI from '../constants/Noti'
 
 const API_URL = `${URL}/notification`
-const appVersion = '0.0.94'
+const appVersion = '0.0.95'
 
 export const checkVersion = () => async dispatch => {
   try {
