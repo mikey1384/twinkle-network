412b2a1558e586d4c818cc3a16092eff06bdc23f
mikey1384 <mikey1384@gmail.com>
2018-03-30 19:54:04 +0700

    remove docker folder

--- .docker/init_mysql.sh
+++
@@ -1,2 +0,0 @@
-mysql_container_id=`docker ps | grep twinkle_mysql | cut -f 1 -d " "`
-docker exec -it $mysql_container_id scripts/init.sh
--- .docker/mysql-scripts/init.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-/config/scripts/createdb.sh
-
-/config/scripts/migratedb.sh /config/migration/db.sql
