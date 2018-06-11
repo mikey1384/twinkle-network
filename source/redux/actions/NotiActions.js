60e4ac4bacb8345d043abb14c852bcef0c050e54
mikey1384 <mikey1384@gmail.com>
2018-06-11 15:21:23 +0700

    added edit and delete buttons for status msg

--- source/redux/actions/NotiActions.js
+++ source/redux/actions/NotiActions.js
@@ -4,7 +4,7 @@ import { URL } from 'constants/URL'
 import NOTI from '../constants/Noti'
 
 const API_URL = `${URL}/notification`
-const appVersion = '0.1.12'
+const appVersion = '0.1.13'
 
 export const checkVersion = () => async dispatch => {
   try {
