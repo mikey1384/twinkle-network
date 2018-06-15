5baeb5eda0ce726025a17979af9f56de2fb834f1
mikey1384 <mikey1384@gmail.com>
2018-06-15 19:45:58 +0700

    related videos show in playlist videos page

--- source/redux/actions/NotiActions.js
+++ source/redux/actions/NotiActions.js
@@ -4,7 +4,7 @@ import { URL } from 'constants/URL'
 import NOTI from '../constants/Noti'
 
 const API_URL = `${URL}/notification`
-const appVersion = '0.1.16'
+const appVersion = '0.1.17'
 
 export const checkVersion = () => async dispatch => {
   try {
