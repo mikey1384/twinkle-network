aaf820a1769636925604d824448c543fe1549a7b
mikey1384 <mikey1384@gmail.com>
2018-10-12 12:38:19 +0700

    minor bug fix

--- source/helpers/requestHelpers.js
+++ source/helpers/requestHelpers.js
@@ -400,7 +400,7 @@ export const uploadContent = async({
     );
     return Promise.resolve(data);
   } catch (error) {
-    handleError(error, dispatch);
+    return handleError(error, dispatch);
   }
 };
 
