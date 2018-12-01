7e1fd8a22421d88535ed2995e47e066cf1a2f01a
mikey1384 <mikey1384@gmail.com>
2018-12-01 22:10:52 +0800

    bug fix

--- source/redux/actions/NotiActions.js
+++ source/redux/actions/NotiActions.js
@@ -4,7 +4,7 @@ import { URL } from 'constants/URL';
 import NOTI from '../constants/Noti';
 
 const API_URL = `${URL}/notification`;
-const appVersion = '0.1.64';
+const appVersion = '0.1.65';
 
 export const changeSocketStatus = connected => ({
   type: NOTI.CHANGE_SOCKET_STATUS,
