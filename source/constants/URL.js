57e3e6d94a2236f5b5bb8dbd8d3a2bf9866eedaf
mikey1384 <mikey1384@gmail.com>
2019-01-09 12:29:18 +0800

    remove awsConfig from git ignore

--- source/constants/URL.js
+++ source/constants/URL.js
@@ -1,2 +1,2 @@
-export const URL = 'https://api.twinkle.network';
-// export const URL = 'http://localhost:3500';
+// export const URL = 'https://api.twinkle.network';
+export const URL = 'http://localhost:3500';
