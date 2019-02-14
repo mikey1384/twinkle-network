f298ef66aee154c99758210450312753a0d1da0b
mikey1384 <mikey1384@gmail.com>
2019-02-14 10:11:16 +0700

    minor fix

--- source/components/Forms/TitleDescriptionForm.js
+++ source/components/Forms/TitleDescriptionForm.js
@@ -3,6 +3,7 @@ import PropTypes from 'prop-types';
 import Textarea from 'components/Texts/Textarea';
 import Button from 'components/Button';
 import Input from 'components/Texts/Input';
+import ErrorBoundary from 'components/Wrappers/ErrorBoundary';
 import { stringIsEmpty, addEmoji, finalizeEmoji } from 'helpers/stringHelpers';
 
 TitleDescriptionForm.propTypes = {
@@ -34,74 +35,76 @@ export default function TitleDescriptionForm({
   }, []);
 
   return (
-    <div>
-      <Input
-        autoFocus={autoFocus}
-        placeholder={titlePlaceholder}
-        type="text"
-        value={title}
-        style={{
-          borderColor: title.length > titleMaxChar && 'red',
-          color: title.length > titleMaxChar && 'red'
-        }}
-        onChange={text => setTitle(text)}
-        onKeyUp={event => setTitle(addEmoji(event.target.value))}
-      />
-      {title.length > titleMaxChar && (
-        <small style={{ color: 'red', fontSize: '1.6rem' }}>
-          {`Exceeded title's`} character limit of {titleMaxChar} characters. You
-          can write more in the description field below.
-        </small>
-      )}
-      <div style={{ position: 'relative' }}>
-        <Textarea
+    <ErrorBoundary>
+      <div>
+        <Input
+          autoFocus={autoFocus}
+          placeholder={titlePlaceholder}
+          type="text"
+          value={title}
+          style={{
+            borderColor: title.length > titleMaxChar && 'red',
+            color: title.length > titleMaxChar && 'red'
+          }}
+          onChange={text => setTitle(text)}
+          onKeyUp={event => setTitle(addEmoji(event.target.value))}
+        />
+        {title.length > titleMaxChar && (
+          <small style={{ color: 'red', fontSize: '1.6rem' }}>
+            {`Exceeded title's`} character limit of {titleMaxChar} characters.
+            You can write more in the description field below.
+          </small>
+        )}
+        <div style={{ position: 'relative' }}>
+          <Textarea
+            style={{
+              marginTop: '1rem',
+              color: description.length > descriptionMaxChar && 'red',
+              borderColor: description.length > descriptionMaxChar && 'red'
+            }}
+            minRows={rows}
+            placeholder={descriptionPlaceholder}
+            value={description}
+            onChange={event => setDescription(event.target.value)}
+            onKeyUp={event => setDescription(addEmoji(event.target.value))}
+          />
+        </div>
+        {description.length > descriptionMaxChar && (
+          <small style={{ color: 'red', fontSize: '1.3rem' }}>
+            {descriptionMaxChar} character limit exceeded
+          </small>
+        )}
+        <div
           style={{
             marginTop: '1rem',
-            color: description.length > descriptionMaxChar && 'red',
-            borderColor: description.length > descriptionMaxChar && 'red'
+            display: 'flex',
+            justifyContent: 'center'
           }}
-          minRows={rows}
-          placeholder={descriptionPlaceholder}
-          value={description}
-          onChange={event => setDescription(event.target.value)}
-          onKeyUp={event => setDescription(addEmoji(event.target.value))}
-        />
-      </div>
-      {description.length > descriptionMaxChar && (
-        <small style={{ color: 'red', fontSize: '1.3rem' }}>
-          {descriptionMaxChar} character limit exceeded
-        </small>
-      )}
-      <div
-        style={{
-          marginTop: '1rem',
-          display: 'flex',
-          justifyContent: 'center'
-        }}
-      >
-        <Button
-          transparent
-          style={{ fontSize: '1.7rem', marginRight: '1rem' }}
-          onClick={onClose}
         >
-          Cancel
-        </Button>
-        <Button
-          primary
-          style={{ fontSize: '1.7rem' }}
-          onClick={submitThis}
-          disabled={
-            submitting ||
-            !title ||
-            stringIsEmpty(title) ||
-            title.length > titleMaxChar ||
-            description.length > descriptionMaxChar
-          }
-        >
-          Submit
-        </Button>
+          <Button
+            transparent
+            style={{ fontSize: '1.7rem', marginRight: '1rem' }}
+            onClick={onClose}
+          >
+            Cancel
+          </Button>
+          <Button
+            primary
+            style={{ fontSize: '1.7rem' }}
+            onClick={submitThis}
+            disabled={
+              submitting ||
+              !title ||
+              stringIsEmpty(title) ||
+              title.length > titleMaxChar ||
+              description.length > descriptionMaxChar
+            }
+          >
+            Submit
+          </Button>
+        </div>
       </div>
-    </div>
+    </ErrorBoundary>
   );
 
   async function submitThis(event) {
