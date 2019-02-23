a80da7f800057fd477a721f66ec461f690a717b1
mikey1384 <mikey1384@gmail.com>
2019-02-23 14:56:44 +0700

    minor bug fix

--- source/components/Forms/SelectUploadsForm/index.js
+++ source/components/Forms/SelectUploadsForm/index.js
@@ -2,8 +2,10 @@ import PropTypes from 'prop-types';
 import React from 'react';
 import Selectable from './Selectable';
 import LoadMoreButton from 'components/Buttons/LoadMoreButton';
+import Loading from 'components/Loading';
 
 SelectUploadsForm.propTypes = {
+  loading: PropTypes.bool,
   loadingMore: PropTypes.bool,
   loadMoreUploads: PropTypes.func,
   loadMoreButton: PropTypes.bool,
@@ -17,6 +19,7 @@ SelectUploadsForm.propTypes = {
 export default function SelectUploadsForm({
   uploads,
   selectedUploads,
+  loading,
   loadingMore,
   loadMoreButton,
   onSelect,
@@ -33,22 +36,39 @@ export default function SelectUploadsForm({
         width: '100%'
       }}
     >
-      {uploads.map((upload, index) => {
-        return (
-          <Selectable
-            key={index}
-            item={upload}
-            selected={
-              selectedUploads
-                .map(selected => selected.id)
-                .indexOf(upload.id) !== -1
-            }
-            onSelect={onSelect}
-            onDeselect={onDeselect}
-            type={type}
-          />
-        );
-      })}
+      {loading ? (
+        <Loading />
+      ) : uploads.length === 0 ? (
+        <div
+          style={{
+            width: '100%',
+            display: 'flex',
+            justifyContent: 'center',
+            alignItmes: 'center',
+            fontSize: '2.5rem',
+            padding: '3rem'
+          }}
+        >
+          No Results
+        </div>
+      ) : (
+        uploads.map((upload, index) => {
+          return (
+            <Selectable
+              key={index}
+              item={upload}
+              selected={
+                selectedUploads
+                  .map(selected => selected.id)
+                  .indexOf(upload.id) !== -1
+              }
+              onSelect={onSelect}
+              onDeselect={onDeselect}
+              type={type}
+            />
+          );
+        })
+      )}
       {loadMoreButton && (
         <div
           style={{
--- source/containers/Home/Stories/InputPanel/SubjectInput/AttachContentModal/SelectAttachmentScreen.js
+++ source/containers/Home/Stories/InputPanel/SubjectInput/AttachContentModal/SelectAttachmentScreen.js
@@ -1,131 +1,107 @@
-import React, { Component } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import PropTypes from 'prop-types';
 import SearchInput from 'components/Texts/SearchInput';
 import SelectUploadsForm from 'components/Forms/SelectUploadsForm';
-import Loading from 'components/Loading';
 import { stringIsEmpty } from 'helpers/stringHelpers';
 import { loadUploads, searchContent } from 'helpers/requestHelpers';
 
-export default class SelectAttachmentScreen extends Component {
-  state = {
-    allUploads: [],
-    loadMoreButton: false,
-    searchedUploads: [],
-    searchLoadMoreButton: false,
-    searchText: '',
-    selectedUpload: [],
-    loaded: false
-  };
+SelectAttachmentScreen.propTypes = {
+  onSelect: PropTypes.func.isRequired,
+  onDeselect: PropTypes.func.isRequired,
+  type: PropTypes.string.isRequired
+};
 
-  static propTypes = {
-    onSelect: PropTypes.func.isRequired,
-    onDeselect: PropTypes.func.isRequired,
-    type: PropTypes.string.isRequired
-  };
+export default function SelectAttachmentScreen({ onSelect, onDeselect, type }) {
+  const [allUploads, setAllUploads] = useState([]);
+  const [loadMoreButton, setLoadMoreButton] = useState(false);
+  const [searchedUploads, setSearchedUploads] = useState([]);
+  const [searchLoadMoreButton, setSearchLoadMoreButton] = useState(false);
+  const [searchText, setSearchText] = useState('');
+  const [selectedUpload, setSelectedUpload] = useState([]);
+  const [loaded, setLoaded] = useState(false);
+  const [searching, setSearching] = useState(false);
+  const timerRef = useRef(null);
 
-  timer = null;
+  useEffect(() => {
+    initScreen();
+    async function initScreen() {
+      const { results, loadMoreButton } = await loadUploads({
+        limit: 18,
+        type
+      });
+      setAllUploads(results);
+      setLoadMoreButton(loadMoreButton);
+      setLoaded(true);
+    }
+  }, []);
 
-  async componentDidMount() {
-    const { type } = this.props;
-    const { results, loadMoreButton } = await loadUploads({
-      limit: 18,
-      type
-    });
-    this.setState({
-      allUploads: results,
-      loadMoreButton,
-      loaded: true
-    });
-  }
+  return (
+    <div style={{ width: '100%' }}>
+      <SearchInput
+        placeholder="Search..."
+        autoFocus
+        style={{
+          marginBottom: '2em',
+          width: '50%'
+        }}
+        value={searchText}
+        onChange={onSearchInput}
+      />
+      <SelectUploadsForm
+        loading={!loaded || (!stringIsEmpty(searchText) && searching)}
+        type={type}
+        uploads={!stringIsEmpty(searchText) ? searchedUploads : allUploads}
+        selectedUploads={selectedUpload}
+        loadMoreButton={
+          !stringIsEmpty(searchText) ? searchLoadMoreButton : loadMoreButton
+        }
+        onSelect={upload => {
+          setSelectedUpload([upload]);
+          onSelect(upload);
+        }}
+        onDeselect={() => {
+          setSelectedUpload([]);
+          onDeselect();
+        }}
+        loadMoreUploads={loadMoreUploads}
+      />
+    </div>
+  );
 
-  render() {
-    const {
-      allUploads,
-      loaded,
-      loadMoreButton,
-      searchedUploads,
-      searchText,
-      searchLoadMoreButton,
-      selectedUpload
-    } = this.state;
-    const { onSelect, onDeselect, type } = this.props;
-
-    return (
-      <div style={{ width: '100%' }}>
-        <SearchInput
-          placeholder="Search..."
-          autoFocus
-          style={{
-            marginBottom: '2em',
-            width: '50%'
-          }}
-          value={searchText}
-          onChange={this.onSearchInput}
-        />
-        {!loaded && <Loading />}
-        <SelectUploadsForm
-          type={type}
-          uploads={!stringIsEmpty(searchText) ? searchedUploads : allUploads}
-          selectedUploads={selectedUpload}
-          loadMoreButton={
-            !stringIsEmpty(searchText) ? searchLoadMoreButton : loadMoreButton
-          }
-          onSelect={upload => {
-            this.setState({
-              selectedUpload: [upload]
-            });
-            onSelect(upload);
-          }}
-          onDeselect={() => {
-            this.setState({
-              selectedUpload: []
-            });
-            onDeselect();
-          }}
-          loadMoreUploads={this.loadMoreUploads}
-        />
-      </div>
-    );
-  }
-
-  loadMoreUploads = async() => {
-    const { type } = this.props;
-    const { allUploads, selectedUpload, searchText } = this.state;
+  async function loadMoreUploads() {
     if (!stringIsEmpty(searchText)) {
       const { results, loadMoreButton } = await searchContent({
         filter: type,
         searchText,
-        shownResults: selectedUpload
+        shownResults: searchedUploads
       });
-      this.setState(state => ({
-        searchedUploads: state.searchedUploads.concat(results),
-        searchLoadMoreButton: loadMoreButton
-      }));
+      setSearchedUploads(searchedUploads.concat(results));
+      setSearchLoadMoreButton(loadMoreButton);
     } else {
       const { results, loadMoreButton } = await loadUploads({
         limit: 18,
         type,
         contentId: allUploads[allUploads.length - 1].id
       });
-      this.setState(state => ({
-        allUploads: state.allUploads.concat(results),
-        loadMoreButton
-      }));
+      setAllUploads(allUploads.concat(results));
+      setLoadMoreButton(loadMoreButton);
     }
-  };
+  }
 
-  onSearchInput = text => {
-    clearTimeout(this.timer);
-    this.setState({ searchText: text });
-    this.timer = setTimeout(() => this.search(text), 300);
-  };
-
-  search = async text => {
-    const { type } = this.props;
-    const { results: searchedUploads, loadMoreButton } = await searchContent({
-      filter: type,
-      searchText: text
-    });
-    this.setState({ searchedUploads, searchLoadMoreButton: loadMoreButton });
-  };
+  function onSearchInput(text) {
+    clearTimeout(timerRef.current);
+    setSearchText(text);
+    setSearching(true);
+    setSearchedUploads([]);
+    timerRef.current = setTimeout(() => search(text), 300);
+    async function search(text) {
+      const { results: searchedUploads, loadMoreButton } = await searchContent({
+        filter: type,
+        searchText: text
+      });
+      setSearching(false);
+      setSearchedUploads(searchedUploads);
+      setSearchLoadMoreButton(loadMoreButton);
+    }
+  }
 }
