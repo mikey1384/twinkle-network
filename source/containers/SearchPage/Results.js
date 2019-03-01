3305fdd30728fc4446435a4c3a38c063ad43220f
mikey1384 <mikey1384@gmail.com>
2019-03-01 09:53:08 +0700

    update search page with hooks

--- source/containers/SearchPage/Results.js
+++ source/containers/SearchPage/Results.js
@@ -22,165 +22,159 @@ Results.propTypes = {
   showMoreResults: PropTypes.func.isRequired
 };
 
-function Results() {
+function Results({
+  changeFilter,
+  closeSearch,
+  filter,
+  loadMoreButton,
+  results,
+  searchText,
+  setResults,
+  showMoreResults
+}) {
   const [searching, setSearching] = useState(false);
   const [loadingMore, setLoadingMore] = useState(false);
+  const [firstRun, setFirstRun] = useState(true);
+  const [prevSearchText, setPrevSearchText] = useState(searchText);
+  const [prevFilter, setPrevFilter] = useState(filter);
   const timerRef = useRef(null);
 
-  componentDidMount() {
-    const { filter, results, searchText } = this.props;
-    if (
-      !stringIsEmpty(searchText) &&
-      searchText.length > 1 &&
-      results.length === 0
-    ) {
-      this.setState({ searching: true });
-      this.timer = setTimeout(
-        () => this.searchContent({ filter, searchText }),
-        500
-      );
+  useEffect(() => {
+    return function cleanUp() {
+      clearTimeout(timerRef.current);
+    };
+  }, []);
+
+  useEffect(() => {
+    if (!stringIsEmpty(searchText) && searchText.length > 1) {
+      if (firstRun && results.length === 0) {
+        setResults({ filter, results: [], loadMoreButton: false });
+      }
+      setFirstRun(false);
+      if (
+        (firstRun && results.length === 0) ||
+        searchText !== prevSearchText ||
+        filter !== prevFilter
+      ) {
+        clearTimeout(timerRef.current);
+        setSearching(true);
+        timerRef.current = setTimeout(handleSearchContent, 500);
+      }
+      setPrevSearchText(searchText);
+      setPrevFilter(filter);
     }
-  }
-
-  componentDidUpdate(prevProps) {
-    const { filter, searchText, setResults } = this.props;
-    if (prevProps.searchText !== searchText || prevProps.filter !== filter) {
-      clearTimeout(this.timer);
-      this.setState({ searching: true });
-      setResults({ filter, results: [], loadMoreButton: false });
-      this.timer = setTimeout(
-        () => this.searchContent({ filter, searchText }),
-        500
-      );
+    async function handleSearchContent() {
+      const data = await searchContent({
+        filter,
+        searchText
+      });
+      if (data) setResults({ filter, ...data });
+      return setSearching(false);
     }
-  }
+  }, [searchText, filter]);
 
-  componentWillUnmount() {
-    clearTimeout(this.timer);
-  }
-
-  render() {
-    const { loadingMore, searching } = this.state;
-    const {
-      changeFilter,
-      closeSearch,
-      filter,
-      loadMoreButton,
-      results
-    } = this.props;
-    const availableFilters = ['video', 'url', 'subject'].filter(
-      availableFilter => availableFilter !== filter
-    );
-    return (
-      <div
-        className={css`
-          @media (max-width: ${mobileMaxWidth}) {
-            padding-bottom: 30rem;
-          }
-        `}
-        style={{
-          display: 'flex',
-          flexDirection: 'column',
-          width: '100%',
-          justifyContent: 'center'
-        }}
-      >
-        {searching && <Loading />}
-        {!searching &&
-          results.map(result => (
-            <Result
-              key={result.id}
-              closeSearch={closeSearch}
-              type={filter}
-              result={result}
-            />
-          ))}
-        {!searching && results.length === 0 && (
-          <div
-            style={{
-              fontSize: '2.5rem',
-              fontWeight: 'bold',
-              color: Color.darkerGray(),
-              justifyContent: 'center',
-              height: '40vh',
-              display: 'flex',
-              flexDirection: 'column',
-              alignItems: 'center'
-            }}
-          >
-            <div style={{ textAlign: 'center' }}>
-              <p style={{ textTransform: 'capitalize' }}>{`No ${
-                filter === 'url' ? 'link' : filter
-              }s Found`}</p>
-              <div style={{ marginTop: '1rem', fontSize: '2rem' }}>
-                Search with the same keyword(s) for:
-                {availableFilters.map((availableFilter, index) => (
-                  <p style={{ textTransform: 'capitalize' }} key={index}>
-                    <a
-                      style={{ cursor: 'pointer' }}
-                      onClick={() => changeFilter(availableFilter)}
-                    >{`${
-                      availableFilter === 'url' ? 'link' : availableFilter
-                    }s`}</a>
-                  </p>
-                ))}
-              </div>
-            </div>
-            <CloseText
-              text="Or tap to close"
-              style={{ marginTop: '3rem', marginBottom: '1rem' }}
-            />
-          </div>
-        )}
-        {!searching && loadMoreButton && (
-          <div style={{ paddingBottom: '8rem' }}>
-            <LoadMoreButton
-              filled
-              info
-              loading={loadingMore}
-              onClick={this.loadMoreSearchResults}
-            />
-          </div>
-        )}
-        {!searching && results.length > 0 && (
-          <CloseText
-            className="desktop"
-            style={{
-              position: 'fixed',
-              left: '50%',
-              transform: 'translateX(-50%)',
-              bottom: '1rem',
-              padding: '1.5rem',
-              borderRadius: borderRadius,
-              background: Color.lightGray(0.8)
-            }}
+  const availableFilters = ['video', 'url', 'subject'].filter(
+    availableFilter => availableFilter !== filter
+  );
+  return (
+    <div
+      className={css`
+        @media (max-width: ${mobileMaxWidth}) {
+          padding-bottom: 30rem;
+        }
+      `}
+      style={{
+        display: 'flex',
+        flexDirection: 'column',
+        width: '100%',
+        justifyContent: 'center'
+      }}
+    >
+      {searching && <Loading />}
+      {!searching &&
+        searchText.length > 1 &&
+        results.map(result => (
+          <Result
+            key={result.id}
+            closeSearch={closeSearch}
+            type={filter}
+            result={result}
           />
-        )}
-      </div>
-    );
-  }
+        ))}
+      {!searching && results.length === 0 && (
+        <div
+          style={{
+            fontSize: '2.5rem',
+            fontWeight: 'bold',
+            color: Color.darkerGray(),
+            justifyContent: 'center',
+            height: '40vh',
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'center'
+          }}
+        >
+          <div style={{ textAlign: 'center' }}>
+            <p style={{ textTransform: 'capitalize' }}>{`No ${
+              filter === 'url' ? 'link' : filter
+            }s Found`}</p>
+            <div style={{ marginTop: '1rem', fontSize: '2rem' }}>
+              Search with the same keyword(s) for:
+              {availableFilters.map((availableFilter, index) => (
+                <p style={{ textTransform: 'capitalize' }} key={index}>
+                  <a
+                    style={{ cursor: 'pointer' }}
+                    onClick={() => changeFilter(availableFilter)}
+                  >{`${
+                    availableFilter === 'url' ? 'link' : availableFilter
+                  }s`}</a>
+                </p>
+              ))}
+            </div>
+          </div>
+          <CloseText
+            text="Or tap to close"
+            style={{ marginTop: '3rem', marginBottom: '1rem' }}
+          />
+        </div>
+      )}
+      {!searching && loadMoreButton && (
+        <div style={{ paddingBottom: '8rem' }}>
+          <LoadMoreButton
+            filled
+            info
+            loading={loadingMore}
+            onClick={loadMoreSearchResults}
+          />
+        </div>
+      )}
+      {!searching && results.length > 0 && (
+        <CloseText
+          className="desktop"
+          style={{
+            position: 'fixed',
+            left: '50%',
+            transform: 'translateX(-50%)',
+            bottom: '1rem',
+            padding: '1.5rem',
+            borderRadius: borderRadius,
+            background: Color.lightGray(0.8)
+          }}
+        />
+      )}
+    </div>
+  );
 
-  searchContent = async({ filter, searchText }) => {
-    const { setResults } = this.props;
-    const data = await searchContent({
-      filter,
-      searchText
-    });
-    if (data) setResults({ filter, ...data });
-    return this.setState({ searching: false });
-  };
-
-  loadMoreSearchResults = async() => {
-    const { filter, results, searchText, showMoreResults } = this.props;
-    this.setState({ loadingMore: true });
+  async function loadMoreSearchResults() {
+    setLoadingMore(true);
     const data = await searchContent({
       filter,
       searchText,
       shownResults: results
     });
     if (data) showMoreResults(data);
-    this.setState({ loadingMore: false });
-  };
+    setLoadingMore(false);
+  }
 }
 
 export default connect(
