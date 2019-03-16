dbdd685b99a357b121aa7282b0b157d3e30130a8
mikey1384 <mikey1384@gmail.com>
2019-03-16 18:22:12 +0700

    bug fix

--- source/constants/defaultValues.js
+++ source/constants/defaultValues.js
@@ -4,7 +4,7 @@ export const rewardValue = {
 };
 import { Color } from './css';
 
-export const clientVersion = '0.2.15';
+export const clientVersion = '0.2.16';
 export const charLimit = {
   chat: {
     subject: 200,
