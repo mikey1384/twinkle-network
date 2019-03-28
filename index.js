caf176ed27d3587aa66c52b48a821665d8bb2dc8
mikey1384 <mikey1384@gmail.com>
2019-03-29 00:34:04 +0700

    more fix

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ const path = require('path');
 const app = express();
 if (process.env.NODE_ENV === 'production') {
   const server = require('./webpack/webpack.prod').default(app);
+  server.use(express.static(path.resolve(__dirname, './public')));
   app.get('*', (req, res) => {
     res.sendFile(path.resolve(__dirname, './public', '/index.html'));
   });
