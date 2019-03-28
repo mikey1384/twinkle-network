9cabc7b36d0b5271c7db6b7823e2828e9fcde499
mikey1384 <mikey1384@gmail.com>
2019-03-28 23:51:54 +0700

    more changes

--- index.js
+++ index.js
@@ -6,7 +6,6 @@ const express = require('express');
 const path = require('path');
 const app = express();
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, 'public')));
   app.get('*', (req, res) => {
     res.sendFile(path.resolve(__dirname, 'public', 'index.html'));
   });
