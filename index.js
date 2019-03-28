aba3f38125e96bd16ab5b5b5d62801c8a092cbac
mikey1384 <mikey1384@gmail.com>
2019-03-29 01:47:33 +0700

    more tries

--- index.js
+++ index.js
@@ -6,36 +6,11 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.resolve(__dirname, 'build')));
-  app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'build', 'index.html'));
-  });
-  require('greenlock-express')
-    .create({
-      version: 'draft-12',
-      configDir: '~/.config/acme',
-      server: 'https://acme-v02.api.letsencrypt.org/directory',
-      approveDomains: function approveDomains(opts, certs, cb) {
-        if (certs) {
-          opts.domains = certs.altnames;
-        } else {
-          opts.email = 'mikey1384@gmail.com';
-          opts.agreeTos = true;
-        }
+const server = require('./webpack/webpack.dev').default(app);
+server.use(express.static(path.resolve(__dirname, './public')));
+const http = require('http');
+const DEV_PORT = process.env.PORT;
 
-        cb(null, { options: opts, certs });
-      },
-      app: app
-    })
-    .listen(80, 443);
-} else {
-  const server = require('./webpack/webpack.dev').default(app);
-  server.use(express.static(path.resolve(__dirname, './public')));
-  const http = require('http');
-  const DEV_PORT = process.env.PORT;
-
-  http.createServer(server).listen(DEV_PORT, function() {
-    console.log('Server listening on port:', DEV_PORT);
-  });
-}
+http.createServer(server).listen(DEV_PORT, function() {
+  console.log('Server listening on port:', DEV_PORT);
+});
---
+++ server.js
@@ -0,0 +1,30 @@
+'use strict';
+
+require('@babel/register');
+require('@babel/polyfill');
+const express = require('express');
+const path = require('path');
+const app = express();
+
+app.use(express.static(path.resolve(__dirname, 'build')));
+app.get('*', (req, res) => {
+  res.sendFile(path.resolve(__dirname, 'build', 'index.html'));
+});
+require('greenlock-express')
+  .create({
+    version: 'draft-12',
+    configDir: '~/.config/acme',
+    server: 'https://acme-v02.api.letsencrypt.org/directory',
+    approveDomains: function approveDomains(opts, certs, cb) {
+      if (certs) {
+        opts.domains = certs.altnames;
+      } else {
+        opts.email = 'mikey1384@gmail.com';
+        opts.agreeTos = true;
+      }
+
+      cb(null, { options: opts, certs });
+    },
+    app: app
+  })
+  .listen(80, 443);
