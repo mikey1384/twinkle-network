e984d80d68c5d5d8d03a858ec139d0beb9138458
mikey1384 <mikey1384@gmail.com>
2019-03-29 00:16:21 +0700

    minor fix

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ require('@babel/polyfill');
 const express = require('express');
 const path = require('path');
 const app = express();
-app.use(express.static(path.resolve(__dirname, '../public')));
+app.use(express.static(path.resolve(__dirname, './public')));
 if (process.env.NODE_ENV === 'production') {
   app.get('*', (req, res) => {
     res.sendFile(path.resolve(__dirname, 'public', 'index.html'));
