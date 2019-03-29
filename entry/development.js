56d225f0980ac4604a5e880188a4f2409c57e26b
mikey1384 <mikey1384@gmail.com>
2019-03-29 15:38:51 +0700

    first stable release

--- entry/development.js
+++ entry/development.js
@@ -9,8 +9,7 @@ const app = express();
 const server = require('../webpack/webpack.dev').default(app);
 server.use(express.static(path.resolve(__dirname, '../public')));
 const http = require('http');
-const DEV_PORT = process.env.PORT;
 
-http.createServer(server).listen(DEV_PORT, function() {
-  console.log('Server listening on port:', DEV_PORT);
+http.createServer(server).listen(process.env.PORT, function() {
+  console.log('Server listening on port:', process.env.PORT);
 });
