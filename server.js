2527bc3bcc319f8afc857eda35c6d6ca5621b5e9
mikey1384 <mikey1384@gmail.com>
2019-03-29 10:27:12 +0700

    more tweaks

--- server.js
+++ server.js
@@ -6,9 +6,10 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
-app.use(express.static(path.resolve(__dirname, 'public')));
+app.use(express.static(path.resolve(__dirname, 'public')), {
+  maxAge: '1y'
+});
 app.get('*', (req, res) => {
-  res.set('Cache-Control', 'public, max-age=31557600');
   res.sendFile(path.resolve(__dirname, 'public', 'index.html'));
 });
 require('greenlock-express')
