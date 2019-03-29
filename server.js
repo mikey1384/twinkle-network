7ffef05e7f308f9d2c3aaf4e09d30e5e8faa9b90
mikey1384 <mikey1384@gmail.com>
2019-03-29 10:33:45 +0700

    minor fix

--- server.js
+++ server.js
@@ -6,11 +6,7 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
-app.use(
-  express.static(path.resolve(__dirname, 'public'), {
-    maxAge: '1y'
-  })
-);
+app.use(express.static(path.resolve(__dirname, 'public')));
 app.get('*', (req, res) => {
   res.sendFile(path.resolve(__dirname, 'public', 'index.html'));
 });
