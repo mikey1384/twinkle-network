ff715c9f916b553662d366097452321172f7e9b2
mikey1384 <mikey1384@gmail.com>
2019-03-29 10:16:46 +0700

    more tweaks

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const app = express();
 app.use(express.static(path.resolve(__dirname, 'public')));
 app.get('*', (req, res) => {
   res.sendFile(path.resolve(__dirname, 'public', 'index.html'), {
-    maxAge: 31536000
+    maxAge: '1y'
   });
 });
 require('greenlock-express')
