89da4255b0c30cd9c257762c5a1daa2cbe485e97
mikey1384 <mikey1384@gmail.com>
2019-03-31 23:39:47 +0700

    version up

--- source/constants/defaultValues.js
+++ source/constants/defaultValues.js
@@ -4,7 +4,7 @@ export const rewardValue = {
 };
 import { Color } from './css';
 
-export const clientVersion = '0.2.22';
+export const clientVersion = '1.0.1';
 export const charLimit = {
   chat: {
     subject: 200,
