64cc34b22b7a698990629ae9b1f3794c18e74bf3
mikey1384 <mikey1384@gmail.com>
2019-05-21 11:40:39 +0800

    more chess updates

---
+++ source/containers/Chat/MessagesContainer/GameOverMessage.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import ErrorBoundary from 'components/Wrappers/ErrorBoundary';
+import { Color } from 'constants/css';
+
+GameOverMessage.propTypes = {
+  opponentName: PropTypes.string,
+  myId: PropTypes.number.isRequired,
+  winnerId: PropTypes.number.isRequired
+};
+
+export default function GameOverMessage({ myId, opponentName, winnerId }) {
+  return (
+    <ErrorBoundary>
+      <div
+        style={{
+          background: myId === winnerId ? Color.gold() : Color.logoBlue(),
+          fontSize: '2.5rem',
+          display: 'flex',
+          justifyContent: 'center',
+          alignItems: 'center',
+          padding: '1rem',
+          color: '#fff',
+          marginRight: '1rem',
+          marginBottom: '1rem'
+        }}
+      >
+        {myId === winnerId ? (
+          <div style={{ textAlign: 'center' }}>
+            <p>{opponentName} failed to make a move in time...</p>
+            <p style={{ fontWeight: 'bold' }}>You win!</p>
+          </div>
+        ) : (
+          <div style={{ textAlign: 'center' }}>
+            <p>You failed to make a move in time...</p>
+            <p>{opponentName} wins</p>
+          </div>
+        )}
+      </div>
+    </ErrorBoundary>
+  );
+}
