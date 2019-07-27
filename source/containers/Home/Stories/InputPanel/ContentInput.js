907ee62bfc5de11abc7f50fd839fa624e695700e
mikey1384 <mikey1384@gmail.com>
2019-07-27 11:26:15 +0800

    fix minor bug

--- source/containers/Home/Stories/InputPanel/ContentInput.js
+++ source/containers/Home/Stories/InputPanel/ContentInput.js
@@ -281,7 +281,7 @@ function ContentInput({ dispatch, uploadFeedContent }) {
       setUrlHelper(
         urlIsValid || stringIsEmpty(url)
           ? ''
-          : `Enter a URL. A URL is a website's internet address. Twinkle Network's URL is <a href="https://www.twin-kle.com" target="_blank">www.twin-kle.com</a> and <a href="https://www.twinkle.network" target="_blank">www.twinkle.network</a>. You can find a webpage's URL at the <b>top area of your browser</b>. Copy a URL you want to share and paste it to the box above.`
+          : `A URL is a website's internet address. Twinkle Network's URL is <a href="https://www.twin-kle.com" target="_blank">www.twin-kle.com</a> and <a href="https://www.twinkle.network" target="_blank">www.twinkle.network</a>. You can find a webpage's URL at the <b>top area of your browser</b>. Copy a URL you want to share and paste it to the box above.`
       );
       const regex = /\b(http[s]?(www\.)?|ftp:\/\/(www\.)?|www\.){1}/gi;
       setForm(form => ({
