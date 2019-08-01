0ff4440b8f8f4515c8f324f4b7cf49a49cbdd690
mikey1384 <mikey1384@gmail.com>
2019-08-02 02:44:13 +0800

    minor fix

--- source/containers/Chat/Message/TextMessage.js
+++ source/containers/Chat/Message/TextMessage.js
@@ -95,7 +95,7 @@ export default function TextMessage({
             height: 35vw;
             @media (max-width: ${mobileMaxWidth}) {
               width: 100%;
-              height: auto;
+              height: 65vw;
             }
           `}
         >
