f46c08ac1ed072ad03dbc032e4fc432120ff4205
mikey1384 <mikey1384@gmail.com>
2019-08-05 16:54:53 +0800

    add reward level form

--- source/components/Modals/RewardLevelModal.js
+++ source/components/Modals/RewardLevelModal.js
@@ -2,8 +2,8 @@ import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import Modal from 'components/Modal';
 import Button from 'components/Button';
-import Icon from 'components/Icon';
 import ErrorBoundary from 'components/Wrappers/ErrorBoundary';
+import RewardLevelForm from 'components/Forms/RewardLevelForm';
 import { connect } from 'react-redux';
 import { setDifficulty } from 'helpers/requestHelpers';
 
@@ -33,50 +33,11 @@ function RewardLevelModal({
           Set Reward Level (consider both difficulty and importance)
         </header>
         <main style={{ fontSize: '3rem', paddingTop: 0 }}>
-          <div style={{ marginTop: '5rem', textAlign: 'center' }}>
-            <div style={{ display: 'flex' }}>
-              <Icon
-                key={0}
-                icon={difficulty > 0 ? 'star' : ['far', 'star']}
-                style={{ cursor: 'pointer' }}
-                onClick={() => setDisplayDifficulty(1)}
-              />
-              <Icon
-                key={1}
-                icon={difficulty > 1 ? 'star' : ['far', 'star']}
-                style={{ cursor: 'pointer' }}
-                onClick={() => setDisplayDifficulty(2)}
-              />
-              <Icon
-                key={2}
-                icon={difficulty > 2 ? 'star' : ['far', 'star']}
-                style={{ cursor: 'pointer' }}
-                onClick={() => setDisplayDifficulty(3)}
-              />
-              <Icon
-                key={3}
-                icon={difficulty > 3 ? 'star' : ['far', 'star']}
-                style={{ cursor: 'pointer' }}
-                onClick={() => setDisplayDifficulty(4)}
-              />
-              <Icon
-                key={4}
-                icon={difficulty > 4 ? 'star' : ['far', 'star']}
-                style={{ cursor: 'pointer' }}
-                onClick={() => setDisplayDifficulty(5)}
-              />
-            </div>
-            <a
-              style={{
-                cursor: 'pointer',
-                fontSize: '1.5rem',
-                userSelect: 'none'
-              }}
-              onClick={() => setDisplayDifficulty(0)}
-            >
-              Clear
-            </a>
-          </div>
+          <RewardLevelForm
+            difficulty={difficulty}
+            onSetDifficulty={setDisplayDifficulty}
+            style={{ marginTop: '5rem', textAlign: 'center' }}
+          />
         </main>
         <footer>
           <Button
