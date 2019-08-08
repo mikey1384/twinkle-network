baecfee33d201d5207a5fca9249883d83318d47f
mikey1384 <mikey1384@gmail.com>
2019-08-09 00:59:52 +0800

    version up

--- source/constants/defaultValues.js
+++ source/constants/defaultValues.js
@@ -5,7 +5,7 @@ export const rewardValue = {
 import { Color } from './css';
 
 export const cloudFrontURL = 'https://d3jvoamd2k4p0s.cloudfront.net';
-export const clientVersion = '1.0.56';
+export const clientVersion = '1.0.57';
 export const charLimit = {
   chat: {
     subject: 200,
