b80806e4fba55d9b4ed565b21e99793a44806922
mikey1384 <mikey1384@gmail.com>
2019-09-19 09:59:38 +0800

    add more emojis

--- source/constants/defaultValues.js
+++ source/constants/defaultValues.js
@@ -5,7 +5,7 @@ export const rewardValue = {
 import { Color } from './css';
 
 export const cloudFrontURL = 'https://d3jvoamd2k4p0s.cloudfront.net';
-export const clientVersion = '1.0.78';
+export const clientVersion = '1.0.79';
 export const charLimit = {
   chat: {
     subject: 200,
