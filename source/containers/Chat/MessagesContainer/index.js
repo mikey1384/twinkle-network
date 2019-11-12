e8ce84c5dc7c4da498c7594f99ca5fbea61beffc
mikey1384 <mikey1384@gmail.com>
2019-11-12 10:12:04 +0900

    increase max file transfer size for regular users up to 500 MB

--- source/containers/Chat/MessagesContainer/index.js
+++ source/containers/Chat/MessagesContainer/index.js
@@ -114,7 +114,7 @@ export default function MessagesContainer({
       ? 4000 * mb
       : authLevel === 4
       ? 2000 * mb
-      : 400 * mb;
+      : 500 * mb;
   const menuProps = currentChannel.twoPeople
     ? [{ label: 'Hide Chat', onClick: handleHideChat }]
     : [
