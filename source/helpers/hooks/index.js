9575ad7dcceb45ef5bb417648f6b0355524e8ef7
mikey1384 <mikey1384@gmail.com>
2019-11-29 12:55:46 +0800

    minor fix

--- source/helpers/hooks/index.js
+++ source/helpers/hooks/index.js
@@ -71,7 +71,7 @@ export function useLazyLoad({
       }
     };
     // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [PanelRef.current, inView]);
+  }, [PanelRef]);
 
   useEffect(() => {
     return function cleanUp() {
