6aade6062899eded9b73d1ef77c6eac896d7c85a
mikey1384 <mikey1384@gmail.com>
2020-01-05 08:15:15 +0800

    like button update

--- source/containers/VideoPage/Details/index.js
+++ source/containers/VideoPage/Details/index.js
@@ -209,7 +209,7 @@ export default function Details({
               className={css`
                 width: CALC(100% - 25rem);
                 @media (max-width: ${mobileMaxWidth}) {
-                  width: CALC(100% - 17rem);
+                  width: CALC(100% - ${canStar ? '16rem' : '13rem'});
                 }
               `}
               style={{
@@ -268,7 +268,7 @@ export default function Details({
               className={css`
                 width: 25rem;
                 @media (max-width: ${mobileMaxWidth}) {
-                  width: 17rem;
+                  width: ${canStar ? '16rem' : '13rem'};
                 }
               `}
               style={{
