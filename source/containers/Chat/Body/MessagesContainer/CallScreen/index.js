d9cb443edad933f5b3fbee2e435e90e1b796f106
mikey1384 <mikey1384@gmail.com>
2020-02-29 12:55:50 +0900

    minor fix

--- source/containers/Chat/Body/MessagesContainer/CallScreen/index.js
+++ source/containers/Chat/Body/MessagesContainer/CallScreen/index.js
@@ -51,6 +51,7 @@ export default function CallScreen({ channelOnCall, style }) {
             { urls: 'stun:stun2.l.google.com:19302' },
             { urls: 'stun:stun3.l.google.com:19302' },
             { urls: 'stun:stun4.l.google.com:19302' },
+            { urls: 'stun:stun.l.google.com:19302' },
             { urls: 'stun:stun.stunprotocol.org:3478' },
             { urls: 'stun:global.stun.twilio.com:3478?transport=udp' }
           ]
