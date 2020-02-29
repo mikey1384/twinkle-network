70e8a1ca4b1951b067c0b6eaa321af37e54fcf5d
mikey1384 <mikey1384@gmail.com>
2020-02-29 13:01:34 +0900

    minor fix

--- source/containers/App/Header/index.js
+++ source/containers/App/Header/index.js
@@ -194,13 +194,12 @@ export default function Header({
         peerRef.current = new Peer({
           config: {
             iceServers: [
-              { urls: 'stun:stun1.l.google.com:19302' },
-              { urls: 'stun:stun2.l.google.com:19302' },
-              { urls: 'stun:stun3.l.google.com:19302' },
-              { urls: 'stun:stun4.l.google.com:19302' },
-              { urls: 'stun:stun.l.google.com:19302' },
-              { urls: 'stun:stun.stunprotocol.org:3478' },
-              { urls: 'stun:global.stun.twilio.com:3478?transport=udp' }
+              {
+                urls: [
+                  'stun:stun1.l.google.com:19302',
+                  'stun:stun2.l.google.com:19305'
+                ]
+              }
             ]
           },
           initiator: false,
--- source/containers/Chat/Body/MessagesContainer/CallScreen/index.js
+++ source/containers/Chat/Body/MessagesContainer/CallScreen/index.js
@@ -47,13 +47,16 @@ export default function CallScreen({ channelOnCall, style }) {
       peerRef.current = new Peer({
         config: {
           iceServers: [
-            { urls: 'stun:stun1.l.google.com:19302' },
-            { urls: 'stun:stun2.l.google.com:19302' },
-            { urls: 'stun:stun3.l.google.com:19302' },
-            { urls: 'stun:stun4.l.google.com:19302' },
-            { urls: 'stun:stun.l.google.com:19302' },
-            { urls: 'stun:stun.stunprotocol.org:3478' },
-            { urls: 'stun:global.stun.twilio.com:3478?transport=udp' }
+            {
+              urls: [
+                {
+                  urls: [
+                    'stun:stun1.l.google.com:19302',
+                    'stun:stun2.l.google.com:19305'
+                  ]
+                }
+              ]
+            }
           ]
         },
         initiator: true,
