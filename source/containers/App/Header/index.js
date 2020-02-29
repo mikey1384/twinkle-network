62ec63369d7e09e3f634ef91add399df3fc77d09
mikey1384 <mikey1384@gmail.com>
2020-02-29 12:56:31 +0900

    minor fix

--- source/containers/App/Header/index.js
+++ source/containers/App/Header/index.js
@@ -198,6 +198,7 @@ export default function Header({
               { urls: 'stun:stun2.l.google.com:19302' },
               { urls: 'stun:stun3.l.google.com:19302' },
               { urls: 'stun:stun4.l.google.com:19302' },
+              { urls: 'stun:stun.l.google.com:19302' },
               { urls: 'stun:stun.stunprotocol.org:3478' },
               { urls: 'stun:global.stun.twilio.com:3478?transport=udp' }
             ]
