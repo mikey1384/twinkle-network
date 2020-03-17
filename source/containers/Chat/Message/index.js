356d521cb09dbbb56d1dc5867897c91f1632d0c1
mikey1384 <mikey1384@gmail.com>
2020-03-17 12:59:53 +0900

    minor fix

--- source/containers/Chat/Message/index.js
+++ source/containers/Chat/Message/index.js
@@ -335,7 +335,7 @@ function Message({
 
   return (
     <div ref={ComponentRef}>
-      {inView || typeof visible === 'undefined' || visible ? (
+      {inView || typeof visible === 'undefined' || visible || isLastMsg ? (
         <div ref={PanelRef} className={MessageStyle.container}>
           <div className={MessageStyle.profilePic}>
             <ProfilePic
