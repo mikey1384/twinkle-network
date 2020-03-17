76566ef23755b6db3e504e112ac71dac857f585c
mikey1384 <mikey1384@gmail.com>
2020-03-17 20:56:09 +0900

    minor bug fix

--- source/helpers/stringHelpers.js
+++ source/helpers/stringHelpers.js
@@ -728,8 +728,8 @@ export function trimWhiteSpaces(text) {
   return newText;
 }
 
-export function truncateText({ text, limit }) {
-  if (text.length > limit) {
+export function truncateText({ text = '', limit }) {
+  if (text?.length > limit) {
     return text.substring(0, limit) + '...';
   }
   return text;
