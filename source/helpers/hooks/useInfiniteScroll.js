3d4b3b3354dea26b1317c8a31c915377e866f86e
mikey1384 <mikey1384@gmail.com>
2020-03-25 09:55:08 +0900

    minor change

--- source/helpers/hooks/useInfiniteScroll.js
+++ source/helpers/hooks/useInfiniteScroll.js
@@ -62,9 +62,9 @@ export default function useInfiniteScroll({
         if (
           loadable &&
           (scrollPositionRef.current.desktop >=
-            scrollHeightRef.current - window.innerHeight - 2000 ||
+            scrollHeightRef.current - window.innerHeight - 3000 ||
             scrollPositionRef.current.mobile >=
-              scrollHeightRef.current - window.innerHeight - 2000)
+              scrollHeightRef.current - window.innerHeight - 3000)
         ) {
           onScrollToBottom();
         }
