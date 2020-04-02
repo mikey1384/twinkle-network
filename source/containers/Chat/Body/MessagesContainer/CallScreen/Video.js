af61b2712c0438755e366148346d978c710e988b
mikey1384 <mikey1384@gmail.com>
2020-04-03 01:08:20 +0900

    more fix

--- source/containers/Chat/Body/MessagesContainer/CallScreen/Video.js
+++ source/containers/Chat/Body/MessagesContainer/CallScreen/Video.js
@@ -5,6 +5,7 @@ import { css } from 'emotion';
 Video.propTypes = {
   stream: PropTypes.object.isRequired
 };
+
 export default function Video({ stream }) {
   const videoRef = useRef(stream);
   useEffect(() => {
