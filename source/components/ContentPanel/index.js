92145e667244d88deddbe49776d0e8148a99020a
mikey1384 <mikey1384@gmail.com>
2020-05-03 22:23:12 +0900

    minor fix

--- source/components/ContentPanel/index.js
+++ source/components/ContentPanel/index.js
@@ -35,6 +35,7 @@ export default function ContentPanel({
   style = {}
 }) {
   const [ComponentRef, inView] = useInView({
+    rootMargin: '100px 0px 0px 0px',
     threshold: 0
   });
   const ContainerRef = useRef(null);
