1faec20419f5401217afc4c60f761763b42ce9d3
mikey1384 <mikey1384@gmail.com>
2020-05-04 00:14:40 +0900

    minor fix

--- source/helpers/hooks/index.js
+++ source/helpers/hooks/index.js
@@ -50,6 +50,11 @@ export function useLazyLoad({
 
   useEffect(() => {
     const clientHeight = PanelRef.current?.clientHeight;
+    if (!prevInView.current && currentInView.current) {
+      if (clientHeight) {
+        onSetPlaceholderHeight(clientHeight);
+      }
+    }
 
     return function onRefresh() {
       if (clientHeight) {
